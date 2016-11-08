8a6f0c17dc846b730c27743250e68579183e5ee1
Sam1luv <eviltoy87@gmail.com>
2016-11-09 01:13:30 +0300

    fixed some mistakes

--- Lesson 1/square.rb
+++ Lesson 1/square.rb
@@ -10,10 +10,10 @@ c = gets.chomp.to_i
 d = b**2 - 4 * (a * c)
 
 if d > 0
-  puts "#{d} x1 = #{(- b + Math.sqrt(d) / (2 * a))} x2 = #{(- b - Math.sqrt(d) / (2 * a))}"
+  puts "#{d}, x1 = #{(- b + Math.sqrt(d) / (2 * a))}, x2 = #{(- b - Math.sqrt(d) / (2 * a))}"
 elsif d == 0
-  puts "#{d} x = #{-b / (2 * a)}"
+  puts "#{d}, x = #{-b / (2 * a)}"
 else
-  puts "#{d} Корней нет"
+  puts "#{d}, Корней нет"
 end
 
