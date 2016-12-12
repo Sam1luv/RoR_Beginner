87f4a8895b06de0c72395dbc636d5698c882650b
Sam1luv <eviltoy87@gmail.com>
2016-12-13 00:14:49 +0300

    add methods to passenger_carriage.rb

--- Lesson_7/passenger_carriage.rb
+++ Lesson_7/passenger_carriage.rb
@@ -1,5 +1,24 @@
 class PassengerCarriage < Carriage
-  def initialize
+  attr_reader :car_capacity
+
+  def initialize(car_capacity)
     @type = :passenger
+    @car_capacity = car_capacity
+  end
+
+  def show_taken_places
+    @car_capacity
+  end
+
+  def show_empty_places
+    # code
+  end
+
+  protected
+
+  attr_writer :car_capacity
+
+  def add_place
+    @car_capacity += 1
   end
 end
