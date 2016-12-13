3ae32ade950e408d7c1a004353a5e7b6b2c90525
Sam1luv <eviltoy87@gmail.com>
2016-12-14 00:43:35 +0300

    add methods to cargo/passenger classes

--- Lesson_7/cargo_carriage.rb
+++ Lesson_7/cargo_carriage.rb
@@ -1,5 +1,27 @@
 class CargoCarriage < Carriage
-  def initialize
+  attr_reader :empty_amount, :occupied_amount
+
+  def initialize(total_amount)
+    @total_amount = total_amount
+    @empty_amount = total_amount
+    @occupied_amount = 0
     @type = :cargo
+  end
+
+  def show_occupied_amount
+    @occupied_amount
+  end
+
+  def show_empty_amount
+    @empty_amount
+  end
+  protected
+
+  attr_writer :empty_amount, :occupied_amount
+
+  def fill_volume!(amount)
+    return if @occupied_amount >= @total_amount
+    @occupied_amount += amount
+    @empty_amount -= amount
   end
 end
--- Lesson_7/passenger_carriage.rb
+++ Lesson_7/passenger_carriage.rb
@@ -1,24 +1,28 @@
 class PassengerCarriage < Carriage
-  attr_reader :car_capacity
+  attr_reader :empty_places, :occupied_places
 
-  def initialize(car_capacity)
+  def initialize(total_places)
     @type = :passenger
-    @car_capacity = car_capacity
+    @total_places = total_places
+    @empty_places = total_places
+    @occupied_places = 0
   end
 
-  def show_taken_places
-    @car_capacity
+  def show_occupied_places
+    @occupied_places
   end
 
   def show_empty_places
-    # code
+    @empty_places
   end
 
   protected
 
-  attr_writer :car_capacity
+  attr_writer :occupied_places, :empty_places
 
-  def add_place
-    @car_capacity += 1
+  def add_place!
+    return if @occupied_places >= @total_places
+    @occupied_places += 1
+    @empty_places -= 1
   end
 end
